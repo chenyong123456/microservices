ca860131d7485dbf1f5b5ffdf98f21c77f2a4f2d
L <296292609@qq.com>
2019-09-27 10:38:02 +0800

    Merge branch 'master' of https://gitee.com/knowimage/GateWay
    # Conflicts:
    #	.gitignore

--- SpringCloudConfig/src/main/java/lyw/demo/DeepSearch.java
+++ SpringCloudConfig/src/main/java/lyw/demo/DeepSearch.java
@@ -3,17 +3,47 @@ package lyw.demo;
 import java.util.*;
 
 public class DeepSearch {
+    private static HashSet<List<String>> hashSet = new HashSet<>();
+
+    //    用来存储一轮深搜后的数据
+    private static HashSet<String> fset = new HashSet<>();
+
     public static void main(String[] args) {
 
 
     }
 
-    public Set<List<String>> deepSearch(Map<String,List<String>> map){
-        HashSet<List<String>> hashSet = new HashSet<List<String>>();
+    public static void deepSearch(Map<String,List<String>> map,String str,HashSet<String> set){
         List<String> list = new ArrayList<>();
 
+        if(set.contains(str)) return;
 
+        set.add(str);
 
-        return null;
+        fset.add(str);
+
+        list = map.get(str);
+
+        list.forEach(s->{
+            deepSearch(map,s,set);
+        });
+
+    }
+
+    public static void merge(Map<String,List<String>> map){
+
+        int count = 0;
+        HashSet<String> set = new HashSet<>();
+
+        for(String key : map.keySet()){
+            deepSearch(map,key,set);
+
+            if(fset.isEmpty()) continue;
+            System.out.println(Arrays.toString(fset.toArray(new String[0])));
+
+            fset.clear();
+            count++;
+        }
+        System.out.println("总条数: " + count);
     }
 }
--- SpringCloudConfig/src/main/java/lyw/demo/calculator.java
+++ SpringCloudConfig/src/main/java/lyw/demo/calculator.java
@@ -9,6 +9,9 @@ import java.util.*;
 public class calculator {
 
     public static void main(String[] args) {
+
+        HashSet<String> keySet = inputKeyWord();
+
         HashMap<String,Integer> map = new HashMap<String,Integer>();
         Scanner scanner = new Scanner(System.in);
         while(scanner.hasNextLine()){
@@ -17,12 +20,16 @@ public class calculator {
             if(StringUtils.isBlank(str)) continue;
             JSONArray jsonArray = JSON.parseArray(str);
             jsonArray.forEach(o -> {
-                if(map.containsKey(o.toString())){
-                    int count = map.get(o.toString());
-                    map.put(o.toString(),++count);
-                }else{
-                    map.put(o.toString(),1);
+                String[] array = o.toString().split("、");
+                for(String s : array){
+                    if(map.containsKey(s)){
+                        int count = map.get(s);
+                        map.put(s,++count);
+                    }else{
+                        map.put(s,1);
+                    }
                 }
+
             });
         }
 
@@ -35,41 +42,117 @@ public class calculator {
         }
 
 
+        test(keySet,list);
+        System.out.println("cal ...");
+        System.out.println(map.size());
+
         HashMap<String,Integer> count_map = new HashMap<>();
 
-        for(int i = 0;i < list.size();++i){
-            String str1 = list.get(i);
-            count_map.put(str1,0);
-            for(int j = 0;j < list.size();++j){
-                if(j == i) continue;
-                String str2 = list.get(j);
-                if(Similiar.similiar(str1,str2) / Math.max(str1.length(),str2.length()) < 0.3){
-                    int count = count_map.get(str1);
-                    count_map.put(str1,count+1);
+//        for(int i = 0;i < list.size();++i){
+//            String str1 = list.get(i);
+//            count_map.put(str1,0);
+//            for(int j = 0;j < list.size();++j){
+//                if(j == i) continue;
+//                String str2 = list.get(j);
+//                if(Similiar.similiar(str1,str2) / Math.max(str1.length(),str2.length()) < 0.3){
+//                    int count = count_map.get(str1);
+//                    count_map.put(str1,count+1);
+//                }
+//            }
+//        }
+
+//        //没有关键字的版本
+//        HashMap<String,List<String>> result_map = new HashMap<>();
+//        for(int i = 0;i < list.size();++i){
+//            String str1 = list.get(i);
+//            result_map.put(str1,new ArrayList<String>());
+//            for(int j = 0;j < list.size();++j){
+//                if(j == i) continue;
+//                String str2 = list.get(j);
+//                if(Similiar.similiar(str1,str2) / Math.max(str1.length(),str2.length()) < 0.4){
+//                    List<String> c_list = result_map.get(str1);
+//                    c_list.add(str2);
+//                }
+//            }
+//            System.out.println("cal: " + list.get(i));
+//        }
+//
+////        计算相似度后得到的结果
+//        for(Map.Entry<String,List<String>> entry : result_map.entrySet()){
+//            List<String> c_list = entry.getValue();
+//            String[] ss = c_list.toArray(new String[0]);
+//            System.out.println(entry.getKey() + " :   " + Arrays.toString(ss));
+//        }
+//
+////        DeepSearch.merge(result_map);
+    }
+
+
+    private static HashSet<String> inputKeyWord(){
+        System.out.println("please input keyword");
+        System.out.println("--------------------------------------------------------------------------------------------------------------------------------------");
+
+        HashSet<String> keySet = new HashSet<>();
+
+        Scanner sc = new Scanner(System.in);
+
+        while(sc.hasNextLine()){
+            String str = sc.nextLine();
+            if(str.equals("123")) break;
+            str = str.substring(0,str.length()-1);
+            keySet.add(str);
+        }
+
+        System.out.println("--------------------------------------------------------------------------------------------------------------------------------------");
+        System.out.println("input finished");
+
+        return keySet;
+    }
+
+    private static void test(HashSet<String> set, List<String> list){
+//        for(String str : set){
+//            List<String> rlist = new ArrayList<>();
+//            list.forEach(s->{
+//                if(Similiar.similiar(s,str) / Math.max(str.length(),s.length()) < 0.3){
+//                    rlist.add(s);
+//                }
+//            });
+//            if(rlist.isEmpty()) continue;
+//            System.out.println(str + ": " + Arrays.toString(rlist.toArray()));
+//            rlist.clear();
+//        }
+
+        HashMap<String,List<String>> map = new HashMap<String,List<String>>();
+
+        double minSimi;
+        String exp = null; //存放当前相似度最高的key
+        for(String str : list){
+            minSimi = 1;
+            exp = null;
+            for (String s : set) {
+                if(str.equals("")) break;
+                double i = Similiar.similiar(s,str) / Math.max(str.length(),s.length());
+                if(i < minSimi) {
+                    minSimi = i;
+                    exp = s;
                 }
+            }
+            if(minSimi > 0.3) continue;
+            if(map.containsKey(exp)){
+                List<String> rlist;
+                rlist = map.get(exp);
+                rlist.add(str);
+                map.put(exp,rlist);
+            }else{
+                List<String> rlist;
+                rlist = new ArrayList<>();
+                rlist.add(str);
+                map.put(exp,rlist);
             }
         }
 
-        HashMap<String,List<String>> result_map = new HashMap<>();
-        boolean f[] = new boolean[list.size()];
-        for(int i = 0;i < list.size();++i){
-            String str1 = list.get(i);
-            result_map.put(str1,new ArrayList<String>());
-            for(int j = 0;j < list.size();++j){
-                if(j == i) continue;
-                String str2 = list.get(j);
-                if(Similiar.similiar(str1,str2) / Math.max(str1.length(),str2.length()) < 0.3){
-                    List<String> c_list = result_map.get(str1);
-                    c_list.add(str2);
-                }
-            }
-        }
-
-        for(Map.Entry<String,Integer> entry : count_map.entrySet()){
-            if(entry.getValue() == 0) continue;
-            List<String> c_list = result_map.get(entry.getKey());
-            String[] ss = c_list.toArray(new String[0]);
-            System.out.println(entry.getKey() + " :" + entry.getValue() + "    " + Arrays.toString(ss));
+        for(Map.Entry<String,List<String>> entry : map.entrySet()){
+            System.out.println(entry.getKey() + ": " + Arrays.toString(entry.getValue().toArray()));
         }
     }
 }
